4ceb1d1cb1fd22a7ee0933915af6a12e4f053a71
hlpdev <hunterpollock2006@gmail.com>
2024-09-12 11:27:31 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,105 @@
+#define GLFW_INCLUDE_GLCOREARB
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <spdlog/spdlog.h>
+#include <glut.h>
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_glfw.h"
+#include "imgui/backends/imgui_impl_glut.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
+
+int main() {
+    spdlog::set_pattern("[%H:%M:%S %z] [%^---%L---%$] [thread %t] %v");
+    spdlog::info("Program start");
+
+    try {
+        glfwSetErrorCallback([](int error, const char* description) {
+            spdlog::error("A GLFW error occurred: %s", description);
+        });
+
+        if (!glfwInit()) {
+            spdlog::error("Failed to initialize GLFW3");
+            return 1;
+        }
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+        // GL ES 2.0 + GLSL 100
+        const char* glsl_version = "#version 100";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+        const char* glsl_version = "#version 150";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+        // GL 3.0 + GLSL 130
+        const char* glsl_version = "#version 130";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+        //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+        GLFWwindow* window = glfwCreateWindow(800, 600, "MacOS OpenGL Rendering via GLFW", nullptr, nullptr);
+        if (!window) {
+            spdlog::error("Failed to create GLFW window");
+            return 1;
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+        io.IniFilename = nullptr;
+
+        ImGui::StyleColorsDark();
+        //ImGui::StyleColorsLight();
+
+        // Setup Platform/Renderer backends
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+#ifdef __EMSCRIPTEN__
+        ImGui_ImplGlfw_InstallEmscriptenCallbacks(window, "#canvas");
+#endif
+        ImGui_ImplOpenGL3_Init(glsl_version);
+
+
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            ImGui::Begin("Test ImGui Window");
+            ImGui::Text("Hello World from CMAKE!");
+            ImGui::End();
+
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(0.2, 0, 0.2, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(window);
+        }
+
+        glfwTerminate();
+    } catch (std::exception& e) {
+        spdlog::critical("Exception thrown: {}", e.what());
+    } catch (std::string& e) {
+        spdlog::critical("Exception thrown: {}", e.c_str());
+    } catch (...) {
+        spdlog::critical("Unknown exception thrown");
+    }
+
+    return 0;
+}
